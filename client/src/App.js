5b2d81409e77721af67cbda2c6ad93ea34cbee6e
jonytalukdar <jonytalukdar30@gmail.com>
2022-03-24 00:53:50 +0600

    added form

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,39 @@
 import React from 'react';
+import memories from './images/memories.png';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import useStyles from './styles';
+import Form from './components/Form/Form';
+import Posts from './components/Posts/Posts';
 
 const App = () => {
+  const classes = useStyles();
+
   return (
-    <div>
-      <h1>app</h1>
-    </div>
+    <Container maxWidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">
+          Memories
+        </Typography>
+        <img className={classes.image} src={memories} alt="icon" height="60" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 };
 
