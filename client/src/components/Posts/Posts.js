228852bba7fe6169b8e7484899be5ce425d67cdd
jonytalukdar <jonytalukdar30@gmail.com>
2022-03-24 16:13:36 +0600

    fetch data

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 const Posts = () => {
+  const { posts } = useSelector((state) => state);
+  console.log(posts);
   return (
     <>
       <h1>Posts</h1>
--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,7 +1,7 @@
 export const postReducer = (state = [], action) => {
   switch (action.type) {
     case 'FETCH_POSTS': {
-      return [...state];
+      return action.payload;
     }
 
     case 'CREATE': {
