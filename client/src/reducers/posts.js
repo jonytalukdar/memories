959d62e2d61adbfedc08889619eabfc18a47dcdc
jonytalukdar <jonytalukdar30@gmail.com>
2022-03-24 15:52:23 +0600

    setup redux

---
+++ client/src/reducers/posts.js
@@ -0,0 +1,14 @@
+export const postReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'FETCH_POSTS': {
+      return [...state];
+    }
+
+    case 'CREATE': {
+      return [...state, action.payload];
+    }
+
+    default:
+      return state;
+  }
+};
