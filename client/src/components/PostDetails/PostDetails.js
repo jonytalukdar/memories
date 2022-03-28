b7c9f1538596aa3f68237f946fc8b0ed02012952
jonytalukdar <jonytalukdar30@gmail.com>
2022-03-28 18:24:08 +0600

    fix css issue

--- client/src/components/PostDetails/PostDetails.js
+++ client/src/components/PostDetails/PostDetails.js
@@ -21,63 +21,62 @@ const PostDetails = () => {
   const history = useHistory();
   const { id } = useParams();
 
-  console.log(post);
-
   useEffect(() => {
     dispatch(getPost(id));
   }, [id, dispatch]);
 
   if (isLoading) {
     return (
-      <Paper className={classes.loadingPaper} elevation={6}>
+      <Paper className={classes.loadingPaper} elevation={5}>
         <CircularProgress size="7em" />
       </Paper>
     );
   }
 
   return (
-    // <h1>details {post.title}</h1>
-    <div className={classes.card}>
-      <div className={classes.section}>
-        <Typography variant="h3" component="h2">
-          {post.title}
-        </Typography>
-        <Typography
-          gutterBottom
-          variant="h6"
-          color="textSecondary"
-          component="h2"
-        >
-          {post.tags.map((tag) => `#${tag} `)}
-        </Typography>
-        <Typography gutterBottom variant="body1" component="p">
-          {post.message}
-        </Typography>
-        <Typography variant="h6">Created by: {post.name}</Typography>
-        <Typography variant="body1">
-          {moment(post.createdAt).fromNow()}
-        </Typography>
-        <Divider style={{ margin: '20px 0' }} />
-        <Typography variant="body1">
-          <strong>Realtime Chat - coming soon!</strong>
-        </Typography>
-        <Divider style={{ margin: '20px 0' }} />
-        <Typography variant="body1">
-          <strong>Comments - coming soon!</strong>
-        </Typography>
-        <Divider style={{ margin: '20px 0' }} />
+    <Paper style={{ borderRadiues: '12px', padding: '20px' }} elevation={5}>
+      <div className={classes.card}>
+        <div className={classes.section}>
+          <Typography variant="h3" component="h2">
+            {post.title}
+          </Typography>
+          <Typography
+            gutterBottom
+            variant="h6"
+            color="textSecondary"
+            component="h2"
+          >
+            {post.tags.map((tag) => `#${tag} `)}
+          </Typography>
+          <Typography gutterBottom variant="body1" component="p">
+            {post.message}
+          </Typography>
+          <Typography variant="h6">Created by: {post.name}</Typography>
+          <Typography variant="body1">
+            {moment(post.createdAt).fromNow()}
+          </Typography>
+          <Divider style={{ margin: '20px 0' }} />
+          <Typography variant="body1">
+            <strong>Realtime Chat - coming soon!</strong>
+          </Typography>
+          <Divider style={{ margin: '20px 0' }} />
+          <Typography variant="body1">
+            <strong>Comments - coming soon!</strong>
+          </Typography>
+          <Divider style={{ margin: '20px 0' }} />
+        </div>
+        <div className={classes.imageSection}>
+          <img
+            className={classes.media}
+            src={
+              post.selectedFile ||
+              'https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png'
+            }
+            alt={post.title}
+          />
+        </div>
       </div>
-      <div className={classes.imageSection}>
-        <img
-          className={classes.media}
-          src={
-            post.selectedFile ||
-            'https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png'
-          }
-          alt={post.title}
-        />
-      </div>
-    </div>
+    </Paper>
   );
 };
 
