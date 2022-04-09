3af0d8c8773debbcd752d80c02d55760cb7c5264
jonytalukdar <jonytalukdar30@gmail.com>
2022-04-09 20:37:48 +0600

    done

--- src copy/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import { authReducer } from './auth';
-import { postReducer } from './posts';
-
-export default combineReducers({
-  posts: postReducer,
-  auth: authReducer,
-});
